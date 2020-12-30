f25d72eb78b183a7bfeac8decc342276bb0398ee
francisco.kennedi <francisco.kennedi@esig.com.br>
2020-12-30 12:39:48 -0300

    301 novo dominio

--- src/server.ts
+++ src/server.ts
@@ -13,13 +13,6 @@ app.use(
   );
 app.use(cors())
 app.use(express.json());
-app.get('*', function(req, res, next) {
-  if (req.headers.host === 'https://catalogueme-backend.herokuapp.com/') {
-    res.redirect('http://www.catalogueme.store' + req.url, 301);
-  } else {
-    next();
-  }
-});
 app.use(routes);
 app.use(express.static('./public'));
 
