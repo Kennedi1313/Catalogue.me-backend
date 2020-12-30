dfad784cab424ed12c4bc8a868f2668d8241956f
francisco.kennedi <francisco.kennedi@esig.com.br>
2020-12-30 10:42:45 -0300

    301 novo dominio

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,16 @@ import routes from './routes';
 
 
 const app = express();
+app.get('/', function(request, response, next) {
+  if(request.headers.host =="https://catalogueme.herokuapp.com") {
+      response.writeHead(301, {'Location':'http://www.catalogueme.store'+ request.url, 'Expires': (new Date).toUTCString()});
+      response.end();
+  } else if(request.headers.host =="catalogueme.herokuapp.com") {
+    response.writeHead(301, {'Location':'http://www.catalogueme.store'+ request.url, 'Expires': (new Date).toUTCString()});
+    response.end();
+} else {
+      next();
+}})
 app.options('*', cors());
 app.use(
     cors({
@@ -15,22 +25,6 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use(express.static('./public'));
-app.get('/', function(request, response, next) {
-  if(request.headers.host =="https://catalogueme.herokuapp.com") {
-      response.writeHead(301, {'Location':'http://www.catalogueme.store'+ request.url, 'Expires': (new Date).toUTCString()});
-      response.end();
-  }
-  else{
-      next();
-  }})
 
-app.get('/', function(request, response, next) {
-  if(request.headers.host =="catalogueme.herokuapp.com") {
-      response.writeHead(301, {'Location':'http://www.catalogueme.store'+ request.url, 'Expires': (new Date).toUTCString()});
-      response.end();
-  }
-  else{
-      next();
-  }})
 
 app.listen(process.env.PORT || 3333);
