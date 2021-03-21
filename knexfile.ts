82d9bac42ed9a6c8686e060739e858b8cd0779b5
francisco.kennedi <francisco.kennedi@esig.com.br>
2021-03-21 16:43:13 -0300

    corrigindo layout

--- knexfile.ts
+++ knexfile.ts
@@ -1,8 +1,16 @@
 
 module.exports = {
     client: "pg",
-    connection: 
-        process.env.DATABASE_URL_SSL || 
+    connection:
+    {
+        user: "wzfrnynfmqhxdk",
+        password: "66370d3eabf7369a31ab838b0df0fbe98d53c69717be90767a2d40f7df100fd2",
+        database: "dfni1lddohhipq",
+        port: 5432,
+        host: "ec2-54-157-78-113.compute-1.amazonaws.com",
+        ssl: true
+    }
+        || 
     {
         database: "catalogueme",
         user: "postgres",
