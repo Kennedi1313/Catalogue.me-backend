17b6fe46039d669145d3a2fd55395e7b49223506
francisco.kennedi <francisco.kennedi@esig.com.br>
2021-03-21 16:51:43 -0300

    corrigindo layout

--- knexfile.ts
+++ knexfile.ts
@@ -7,8 +7,7 @@ module.exports = {
         password: "66370d3eabf7369a31ab838b0df0fbe98d53c69717be90767a2d40f7df100fd2",
         database: "dfni1lddohhipq",
         port: 5432,
-        host: "ec2-54-157-78-113.compute-1.amazonaws.com",
-        ssl: { rejectUnauthorized: false }
+        host: "ec2-54-157-78-113.compute-1.amazonaws.com"
     }
         || 
     {
