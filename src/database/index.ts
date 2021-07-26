a4fbd00ecf0731018c4368d42003a184f6e46afd
kennedi1313 <francisco_kennedi@hotmail.com>
2021-07-26 14:26:54 -0300

    retirando complicações da api

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,9 @@
-import { SSM } from 'aws-sdk';
 import knex from 'knex'
 
 const db = knex({
     client: "pg",
     connection: 
-        {
-            user: "wzfrnynfmqhxdk",
-            password: "66370d3eabf7369a31ab838b0df0fbe98d53c69717be90767a2d40f7df100fd2",
-            database: "dfni1lddohhipq",
-            port: 5432,
-            host: "ec2-54-157-78-113.compute-1.amazonaws.com",
-            ssl: true
-        } 
+            process.env.DATABASE_URL
 });
 
 export default db;
